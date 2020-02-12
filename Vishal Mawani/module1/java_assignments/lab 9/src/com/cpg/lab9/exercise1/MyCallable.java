dc3aa132468a7d4e0d62d58b9c7557e5c6d8c464
VISHAL <vishal162261@gmail.com>
2020-02-12 22:53:10 +0530

    lab9 commit1

---
+++ Vishal Mawani/module1/java_assignments/lab 9/src/com/cpg/lab9/exercise1/MyCallable.java
@@ -0,0 +1,21 @@
+package com.cpg.lab9.exercise1;
+
+
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<String> {
+
+	private long waitTime;
+	
+	public MyCallable(int timeInMillis){
+		this.waitTime=timeInMillis;
+	}
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(waitTime);
+        //return the thread name executing this callable task
+        return Thread.currentThread().getName();
+	}
+
+}
