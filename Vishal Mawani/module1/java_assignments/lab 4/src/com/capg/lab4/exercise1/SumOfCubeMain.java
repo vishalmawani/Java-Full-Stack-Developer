3c3f45d83ac56c27db29c49694c9b2b9f4831c15
VISHAL <vishal162261@gmail.com>
2020-02-12 22:25:51 +0530

    lab4 commit1

---
+++ Vishal Mawani/module1/java_assignments/lab 4/src/com/capg/lab4/exercise1/SumOfCubeMain.java
@@ -0,0 +1,10 @@
+package com.capg.lab4.exercise1;
+
+public class SumOfCubeMain {
+
+	public static void main(String[] args) {
+		SumOfCube obj=new SumOfCube();
+		System.out.println(obj.sumFunction(obj.num));
+	}
+
+}
